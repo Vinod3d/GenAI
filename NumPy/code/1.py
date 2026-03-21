d9a180539b88343348f5c4996baf305bee104bf6
Vinod3d <vinodchandra979@gmail.com>
2026-03-21 10:14:43 +0530

    removed unnecessary file

--- NumPy/code/1.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-a = np.array([1, 2, 3, 4, 5])
-print(a)
-
-python_list = [1, 2, 3, 4, 5]
-print(python_list)
--- NumPy/code/2.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-# Creating a 2D array with 3 rows and 4 columns
-arr = np.zeros((3, 4))
-
-print(arr)
